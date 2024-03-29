2d3a1a47a336b20d79f692273285eccae5adb123
Adarsh <adarsh.hit.aeie25@heritageit.edu.in>
2024-03-30 04:55:44 +0530

    signup page updated

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -61,7 +61,9 @@ export const Signup = () => {
                     lastname,
                   }
                 );
-               
+                localStorage.setItem("token", response.data.token);
+                toast.success("login successful");
+                navigate("/dashboard");
               }}
               label={"Sign up"}
             />
